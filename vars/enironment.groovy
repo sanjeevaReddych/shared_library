0302d3052c3b92e8309e7c2c920cfa34582bcc3e
san <sanjevareddy.ch@gmail.com>
2021-10-05 10:02:57 +0530

    w#2

--- vars/enironment.groovy
+++ vars/enironment.groovy
@@ -1,8 +1,10 @@
-def call(){
+def call(env){
+if (env == 'bazel'){
 echo "Env setup for Bazel"
 sh '''
     sudo apt install npm
     sudo npm install -g @bazel/bazelisk
     bazel --version
   '''
+}
 }
--- vars/environment/bazel.groovy
+++
@@ -1,8 +0,0 @@
-def call(){
-echo "Env setup for Bazel"
-sh '''
-    sudo apt install npm
-    sudo npm install -g @bazel/bazelisk
-    bazel --version
-  '''
-}
