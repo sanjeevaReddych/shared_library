90f20010a1d652411b9cf138d272107587286713
san <sanjevareddy.ch@gmail.com>
2021-10-07 14:56:07 +0530

    refactored for better understanding

---
+++ vars/bazel_helper.groovy
@@ -0,0 +1,19 @@
+def prepare_environment(){
+
+echo "Env setup for Bazel"
+sh '''
+    sudo apt install npm
+    sudo npm install -g @bazel/bazelisk
+    bazel --version
+  '''
+
+}
+
+
+def create_build(dir, build_function){
+
+ sh '''            
+            cd ${dir}
+            bazel build //main:${build_function}
+              '''
+}
